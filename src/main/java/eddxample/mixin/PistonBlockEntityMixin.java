5f48fe9e4c9b4e3123e230e367d3fc633f027d1d
EDDxample <EDDxample@gmail.com>
2019-10-10 15:15:09 +0200

    change honey blocks so they move entities

--- src/main/java/eddxample/mixin/PistonBlockEntityMixin.java
+++ src/main/java/eddxample/mixin/PistonBlockEntityMixin.java
@@ -1,4 +1,28 @@
 package eddxample.mixin;
 
-public class PistonBlockEntityMixin {
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
+import net.minecraft.block.entity.PistonBlockEntity;
+import net.minecraft.class_4623;
+import net.minecraft.util.math.Box;
+import net.minecraft.util.math.Direction;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(PistonBlockEntity.class)
+public abstract class PistonBlockEntityMixin {
+    @Shadow private BlockState pushedBlock;
+
+    @Redirect(method = "pushEntities", at = @At(
+            value = "INVOKE",
+            target = "Lnet/minecraft/class_4623;method_23362(Lnet/minecraft/util/math/Box;Lnet/minecraft/util/math/Direction;D)Lnet/minecraft/util/math/Box;"
+    ))
+    private Box extendMore(Box box_1, Direction direction_1, double double_1) {
+        Box ret = class_4623.method_23362(box_1, direction_1, double_1);
+        if (pushedBlock.getBlock() == Blocks.HONEY_BLOCK) return ret.expand(1);
+        return ret;
+    }
 }
--- src/main/java/eddxample/mixin/PistonMathMixin.java
+++ src/main/java/eddxample/mixin/PistonMathMixin.java
@@ -1,4 +1,21 @@
 package eddxample.mixin;
 
+import net.minecraft.class_4623;
+import net.minecraft.util.math.Box;
+import net.minecraft.util.math.Direction;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.List;
+
+@Mixin(class_4623.class)
 public class PistonMathMixin {
+    @Inject(method = "method_23363", at = @At("RETURN"))
+    private static List<Box> growAABB(boolean bl, Box box, Direction dir, double d, CallbackInfoReturnable<List<Box>> cir) {
+        List<Box> out = cir.getReturnValue();
+        if (bl) out.add(box.expand(0.1));
+        return out;
+    }
 }
